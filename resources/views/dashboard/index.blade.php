4a1a41fb3e78cc66c5cc888541851002b4f1c255
Salem Sayed <salemsayed981@gmail.com>
2023-03-29 05:30:10 +0200

    first commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.dashboard.app')
+
+
+
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>this is dashboard</h1>
+        </section>
+        <section class="content-header">
+            <h1>this is dashboard</h1>
+        </section>
+    </div>
+@endsection
